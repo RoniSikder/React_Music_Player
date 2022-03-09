e32bb448561d8ff512a2aa5c01768652000cf8d0
RoniSikder <ronisikder200@gmail.com>
2022-03-10 01:07:29 +0530

    Second Commit

---
+++ src/components/SongList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export const SongList = () => {
+  return (
+    <div className="container">
+        <div className='songs'>
+            <span>Let Me Love You</span>
+            <Button>Play</Button>    
+        </div>
+        <div className='songs'>
+            <span>Shape of You</span>
+            <Button>Play</Button>    
+        </div>
+        <div className='songs'>
+            <span>Ignite</span>
+            <Button>Play</Button>    
+        </div>
+        <div className='songs'>
+            <span>Lily</span>
+            <Button>Play</Button>    
+        </div>
+        <div className='songs'>
+            <span>Diamond Hearts</span>
+            <Button>Play</Button>    
+        </div>    
+    </div>
+  )
+}
