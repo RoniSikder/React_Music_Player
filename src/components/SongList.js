c54b93e1bd5e8fc615e4a3524aeabd9185b54e29
RoniSikder <ronisikder200@gmail.com>
2022-03-12 23:28:51 +0530

    fourth commit

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Songs } from './Songs';
 
 export const SongList = () => {
     let pos={
@@ -9,28 +10,32 @@ export const SongList = () => {
         justifyContent:"space-between",
         fontSize:"30px"
     }
+    let assign=0;
   return (
+      <>
     <div className="container">
         <div className='songs' style={pos}>
             <span>Let Me Love You</span>
-            <button>Play</button>    
+            <button onClick={assign=0} style={{border:"none",backgroundColor:"transparent"}}>Play</button>    
         </div>
         <div className='songs' style={pos}>
             <span>Shape of You</span>
-            <button>Play</button>    
+            <button onClick={assign=1} style={{border:"none",backgroundColor:"transparent"}}>Play</button>    
         </div>
         <div className='songs' style={pos}>
             <span>Ignite</span>
-            <button>Play</button>    
+            <button onClick={assign=2} style={{border:"none",backgroundColor:"transparent"}}>Play</button>    
         </div>
         <div className='songs' style={pos}>
             <span>Lily</span>
-            <button>Play</button>    
+            <button onClick={assign=3} style={{border:"none",backgroundColor:"transparent"}}>Play</button>    
         </div>
         <div className='songs' style={pos}>
             <span>Diamond Hearts</span>
-            <button>Play</button>    
-        </div>    
+            <button onClick={assign=4} style={{border:"none",backgroundColor:"transparent"}}>Play</button>    
+        </div>
     </div>
+    <Songs/>
+    </>
   )
 }
---
+++ src/components/Songs.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Songs = () => {
+    let songList = [
+        {name: "Let Me Love You", path:"src\Songs\Let Me Love You.mp3"},
+        {name:"Shape Of You",path:"src\Songs\Shape of You.mp3"},
+        {name:"Ignite",path:"src\Songs\Coldplay.mp3"},
+        {name:"Lily",path:"src\Songs\Lily.mp3"},
+        {name:"Diamond Hearts",path:"src\Songs\Diamond Heart.mp3"}
+    ]
+
+    let plays=()=>{
+
+    }
+  return (
+    <>
+
+    </>
+  )
+}
