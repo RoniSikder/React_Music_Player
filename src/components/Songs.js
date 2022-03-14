2ae620a7cc74e9b7b012c132a4b624da3929d5ba
RoniSikder <ronisikder200@gmail.com>
2022-03-14 21:55:03 +0530

    eighth commite

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -5,7 +5,6 @@ import song2 from '../Songs/Coldplay.mp3'
 import song3 from '../Songs/Lily.mp3'
 import song4 from '../Songs/Diamond Heart.mp3'
 
-<<<<<<< HEAD
 
 export const Songs = ({propa}) => {
     let song=null;
@@ -22,24 +21,6 @@ export const Songs = ({propa}) => {
       song = new Audio(songList[propa].path);
       song.play();
     }
-
-
-
-
-=======
-export const Songs = ({propa}) => {
-    let songList = [
-        {name: "Let Me Love You", path:"../Songs/Let Me Love You.mp3"},
-        {name:"Shape Of You",path:"../Songs/Shape of You.mp3"},
-        {name:"Ignite",path:"../Songs/Coldplay.mp3"},
-        {name:"Lily",path:"../Songs/Lily.mp3"},
-        {name:"Diamond Hearts",path:"../Songs/Diamond Heart.mp3"}
-    ]
-    console.log(songList[propa].name);
-    let song=new Audio(songList[propa].path)
-    song.play();
-    
->>>>>>> 77c8cff7281b2b73144c301411627c46e77658fd
   return (
     <>
 
